98214eac6a505e8e18f82fd7ed6540fea70a1f44
Kautuk <kautukpandey2004@gmail.com>
2026-01-19 15:10:13 +0530

    Refactor system info logging to improve readability and structure

--- TASK1601/os_module.js
+++ TASK1601/os_module.js
@@ -1,24 +1,31 @@
-const os = require('os');
-const fs = require('fs')
-const totalMemory = os.totalmem()/(1024*1024*1024);
-const freeMemory = os.freemem()/(1024*1024*1024);
+const fs = require("fs");
+const os = require("os");
 
-const platform = os.platform()
-const cpu = os.cpus()[0].model
+function logSystemInfo() {
+    const totalMemory = os.totalmem() / (1024 * 1024 * 1024);
+    const freeMemory = os.freemem() / (1024 * 1024 * 1024);
+    const platform = os.platform();
+    const cpu = os.cpus()[0].model;
+    const uptime = os.uptime() / 3600;
+    const timestamp = new Date().toLocaleString();
 
-console.log(cpu);
-os.userInfo()
+    const log = `
+Time : ${timestamp}
+Total Memory : ${totalMemory.toFixed(2)} GB
+Free Memory : ${freeMemory.toFixed(2)} GB
+Platform : ${platform}
+CPU : ${cpu}
+Uptime : ${uptime.toFixed(2)} hours
+-----------------------------
+`;
 
-const log = `
-    Time: ${timestamp}
-    free memory: ${freeMemory}    
-`
-
-setInterval(()=>{
-    fs.appendFile("./system_info.txt",log,(err)=>{
-        if(err){
-            console.log(err);
-
+    fs.appendFile("system_info.log", log, (err) => {
+        if (err) {
+            console.log("Failed to write system info log");
+        } else {
+            console.log("System info logged at", timestamp);
         }
-    })
-},5000)
+    });
+}
+
+setInterval(logSystemInfo, 5000);
