3cd4e13962f390e2052cf9fdf8f6e2d0078d0237
Kautuk <kautukpandey2004@gmail.com>
2026-02-11 15:26:59 +0530

    [Kautuk]: Added

--- ExpressFundamentals/public/index.js
+++ ExpressFundamentals/public/index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/register',(req,res)=>{
+    console.log("Form data: ",req.body);
+    res.send("register")
+})
+
+app.post('/contact',(req,res)=>{
+    console.log("Contact data: ",req.body);
+    res.send("contact")
+    
 })
 
 app.listen(PORT,()=>{
