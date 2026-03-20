485d1c552869ee04cab4c47dfa21e9556b904bbc
Kautuk <kautukpandey2004@gmail.com>
2026-03-20 11:41:08 +0530

    Implement updateStudent function and update routes for student data management

--- ExpressFundamentals/controllers/studentController.js
+++ ExpressFundamentals/controllers/studentController.js
@@ -37,6 +37,41 @@ const createStudent = async (req, res) => {
     }
 };
 
+const updateStudent = async (req, res) => {
+    try {
+        const { id } = req.params;   // URL se id aayegi
+        const { name, branch } = req.body;
 
+        let students = await db.readStudentsFromFile();
 
-module.exports = { getAllStudents, createStudent };
+        if (!students || students.length === 0) {
+            return res.status(404).json({ message: "No students found" });
+        }
+
+        // student find karo
+        const index = students.findIndex(s => s.id == id);
+
+        if (index === -1) {
+            return res.status(404).json({ message: "Student not found" });
+        }
+
+        // update fields (jo aaye hain wahi update karo)
+        if (name) students[index].name = name;
+        if (branch) students[index].branch = branch;
+
+        students[index].updatedAt = new Date().toLocaleString();
+
+        // file me wapas likho
+        await db.writeStudentsToFile(students);
+
+        res.status(200).json({
+            message: "Student updated successfully",
+            student: students[index]
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Error updating student" });
+    }
+};
+
+module.exports = { getAllStudents, createStudent , updateStudent };
--- ExpressFundamentals/routes/studentsRoutes.js
+++ ExpressFundamentals/routes/studentsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllStudents,createStudents} = require('../controllers')
+const {getAllStudents,createStudents,updateStudent} = require('../controllers')
 router.get("/",getAllStudents)
 router.post("/",createStudents)
+router.post("/update:id",updateStudent)
 
 module.exports = router
